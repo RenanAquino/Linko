d9e16fac0a395fac6a633bc691d309de8e491f07
RenanAquino <renanaqpython@gmail.com>
2024-07-19 19:35:15 -0300

    feat: Basic Spring setup

---
+++ src/main/java/com/study/friendnet/FriendnetApplication.java
@@ -0,0 +1,13 @@
+package com.study.friendnet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FriendnetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FriendnetApplication.class, args);
+	}
+
+}
